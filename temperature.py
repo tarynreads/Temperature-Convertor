50f2b149d91d8e4f506bcb5827d236226774b11d
Taryn <taryn.sola@gmail.com>
2025-08-19 13:42:58 -0800

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,6 @@
+import temperature
+
+temp = 30
+print(temp, "Celsius =", round(temperature.to_fahrenheit(temp)),"Fahrenheit")
+print(temp, "Fahrenheit =", round(temperature.to_celsius(temp)), "Celsius")
+
